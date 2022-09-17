097443268cd70d71418b6dc631234c3b8f2a3d04
Tsuyoshi Tomozawa <sphdm244@gmail.com>
2022-09-17 16:27:24 +0900

    feat: 投稿処理の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,31 @@
 class PostsController < ApplicationController
   before_action :set_post, only: :index
+  before_action :set_posts, only: [:index, :create]
+
   def index
-    @posts = Post.all
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "投稿しました"
+      redirect_to root_url
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   private
+
   def set_post
     @post = Post.new
+  end
+
+  def set_posts
+    @posts = Post.recent.all
+  end
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
