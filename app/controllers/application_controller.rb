a59d0c792e2c2321f0e5f496d54dd34029eaab32
Tsuyoshi Tomozawa <sphdm244@gmail.com>
2022-10-05 09:13:28 +0900

    feat(application_controller.rb): リファクタリング

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:profile])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:blog_url])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:profile, :blog_url])
   end
 
   def after_sign_out_path_for(resource_or_scope)
