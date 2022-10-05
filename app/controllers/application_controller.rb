1645ae68992be06678c6a76942b1d3825d07882a
Tsuyoshi Tomozawa <sphdm244@gmail.com>
2022-10-05 09:11:02 +0900

    feat: アカウント情報　プロフィールとブログURLを更新できるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:blog_url])
+  end
 
   def after_sign_out_path_for(resource_or_scope)
     login_path
