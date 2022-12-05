9386f865def8d478a3ef1b820f8a2afe9fa8c654
Tsuyoshi Tomozawa <sphdm244@gmail.com>
2022-12-06 08:09:01 +0900

    feat: フォロー機能テスト実装：フォローした投稿の表示関連を実装中

--- spec/system/relationships_spec.rb
+++ spec/system/relationships_spec.rb
@@ -4,7 +4,10 @@ RSpec.describe "Relationships", type: :system do
   before do
     @user = FactoryBot.create(:user, name: "user")
     @other = FactoryBot.create(:user, name: "other")
-    @post = FactoryBot.create(:post, user: @other)
+    @another = FactoryBot.create(:user, name: "another")
+    @post1 = FactoryBot.create(:post, user: @other)
+    @post2 = FactoryBot.create(:post, user: @another)
+    @mypost = FactoryBot.create(:post, user: @user)
   end
 
   describe "フォローとアンフォロー" do
@@ -12,27 +15,44 @@ RSpec.describe "Relationships", type: :system do
       sign_in_as(@user)
       expect(@user.following.size).to eq 0
       expect(@other.followers.size).to eq 0
-      expect(page).to have_button 'フォロー'
+      expect(find("#post-#{@post1.id} input[name='commit']").value).to eq "フォロー"
 
       # フォロー
-      # find("#post-#{@post.id} #new_relationship input[name='commit']").click
-      click_button "フォロー"
-
+      find("#post-#{@post1.id} input[name='commit']").click
       expect(@user.reload.following.size).to eq 1
       expect(@other.reload.followers.size).to eq 1
-      expect(page).to have_button 'アンフォロー'
+      expect(find("#post-#{@post1.id} input[name='commit']").value).to eq "アンフォロー"
 
       # アンフォロー
-      click_button "アンフォロー"
+      find("#post-#{@post1.id} input[name='commit']").click
       expect(@user.reload.following.size).to eq 0
       expect(@other.reload.followers.size).to eq 0
-      expect(page).to have_button 'フォロー'
+      expect(find("#post-#{@post1.id} input[name='commit']").value).to eq "フォロー"
     end
   end
 
   describe "タイムライン" do
-    it 'パラメータなし' do
+    before do
+      sign_in_as(@user)
+      find("#post-#{@post1.id} input[name='commit']").click
+    end
 
+    context 'パラメータなし' do
+      before do
+        visit root_path
+      end
+
+      it 'フォローボタン表示' do
+        expect(find("#post-#{@post2.id} input[name='commit']").value).to eq "フォロー"
+      end
+
+      it 'アンフォローボタン表示' do
+        expect(find("#post-#{@post1.id} input[name='commit']").value).to eq "アンフォロー"
+      end
+
+      it '自分の投稿にはフォローボタンがないことの確認' do
+        expect(has_field?("#post-#{@mypost.id} input[name='commit']")).to be_falsey
+      end
     end
 
     context 'パラメータあり' do
